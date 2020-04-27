2a8ce48b5947848438fcac49e833a9d82e9191bf
Kalib Hackin <nagui08@hotmail.com>
2020-04-26 22:13:25 -0500

    added 2 asynchronis tests

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -4,6 +4,15 @@ const reverseString = (str, callback) => {
   callback(str.split('').reverse().join(''));
 };
 
+const reverseWithPromise = str => {
+  return new Promise((resolve, reject) => {
+    if (!str) {
+      reject(Error('Error'));
+    }
+    resolve(str.split('').reverse().join(''));
+  });
+}
+
 it('The text has to include "world"', () => {
   expect(text).toMatch(/world/);
 });
@@ -24,4 +33,16 @@ it('Testing a callback', () => {
   reverseString('Hello', (str) => {
     expect(str).toBe('olleH');
   })
+});
+
+it('Testing a promise', () => {
+  return reverseWithPromise('Hello')
+    .then(string => {
+      expect(string).toBe('olleH');
+    })
+});
+
+it('Testing a async / await', async () => {
+  const text = await reverseWithPromise('Hello');
+  expect(text).toBe('olleH');
 });
